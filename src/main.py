8fe060fbdc90f28939db09c32f8fffd231edcbd0
bharathichinnusamy <lcsbharathi@gmail.com>
2020-01-21 06:35:00 +0000

    firstone

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,14 +28,42 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/create', methods=['POST'])
+def handle_create():
+    userdata=request.get_json()
+    obj1=Person(username=userdata["username"],email=userdata["email"],address=userdata["address"],phone=userdata["phone"])
+    db.session.add(obj1)
+    db.session.commit()
+    return "Successfully inserted the data"
 
-    response_body = {
-        "hello": "world"
-    }
+@app.route('/get')
+def handle_get():
+    userdata1=Person.query.all()
+    allpeople = list(map(lambda x: x.serialize(), userdata1))
+    return jsonify(allpeople)
+        
+@app.route('/update/<id>',methods=['PUT'])
+def handle_update(id):
+    newobj=Person.query.get(id)
+    newobj1=request.get_json()
+    if 'username' in newobj1:
+        newobj.username=newobj1["username"]
+    if 'email' in newobj1:
+        newobj.email=newobj1["email"]
+    if 'address' in newobj1:
+        newobj.address=newobj1["address"]
+    if 'phone' in newobj1:
+        newobj.phone=newobj1["phone"]
+    db.session.merge(newobj)
+    db.session.commit()
+    return "updated is over"
 
-    return jsonify(response_body), 200
+@app.route('/delete/<id>',methods=['DELETE'])
+def handle_delete(id):
+    userdata3=Person.query.get(id)
+    db.session.delete(userdata3)
+    db.session.commit()
+    return "deleted is done"
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
